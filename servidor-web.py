29fd9f0ceb33fd3e348002416eb3cb1c42f2e3ad
loaspra <santiago.madariaga@utec.edu.pe>
2022-12-12 00:46:17 -0500

    Instrucciones y tareas diciembre

--- servidor-web.py
+++ servidor-web.py
@@ -1,70 +1,83 @@
 """
-servidor-web.py
-
 Script que muestra las peticiones mandadas por el proceso en la interfaz
 
-El trabajo para ahora (26 octubre) consiste en diseñar el layout de la interfaz
-para mostrar los graficos.
+
+
 """
-# para ver la interfaz:
+
 # visit http://127.0.0.1:8050/ in your web browser.
-
-
-from dash import Dash, html, dcc
-from flask import Flask, request, jsonify
-import plotly.express as px
+import dash
+from dash.dependencies import Input, Output
+from dash import dcc, html
+import json
+import redis
+import os
 import pandas as pd
 
-app = Dash(__name__)
+# este es el script que inserta datos artificiales a redis
+import tasks
 
-server = Flask(__name__)
+app = dash.Dash("cansat-ui")
+server = app.server
 
-@server.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
-def add_message(uuid):
-    content = request.json
-    print(content['mytext'])
-    return jsonify({"uuid":uuid})
+# initialize the data when the app starts
+tasks.update_data()
 
-"""
-Codigo de ejemplo
-"""
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-# figura (grafico de barras)
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+# connect with redis-server
+# en mi caso, el server esta corriendo en WSL
+REDIS_URL = "redis://127.0.0.1:6379"
+redis_instance = redis.StrictRedis.from_url(REDIS_URL)
 
-"""
-la distribucion de la interfaz se lleva a cabo usando componentes HTML
-  como divs, las graficas se manejan con la libreria Dash.dcc (dash core 
-  components: https://dash.plotly.com/dash-core-components/graph)
-"""
-app.layout = html.Div(children=[
-    # header de la app
-    html.H1(children='Hello Dash'),
-    # subtitulo
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-    # grafico
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    ),
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-    # grafico
-    dcc.Graph(
-        id='graph2',
-        figure=fig
+
+def serve_layout():
+    return html.Div(
+        [
+            dcc.Interval(interval=2 * 1000, id="interval"),
+            html.H1("Redis, Celery, and Periodic Updates"),
+            html.Div(id="status"),
+            dcc.Graph(id="graph"),
+        ]
     )
-])
 
-if __name__ == '__main__':
+
+app.layout = serve_layout
+
+
+def get_dataframe():
+    """Retrieve the dataframe from Redis
+    This dataframe is periodically updated through the redis task
+    """
+    jsonified_df = redis_instance.hget(
+        tasks.REDIS_HASH_NAME, tasks.REDIS_KEYS["DATASET"]
+    ).decode("utf-8")
+    df = pd.DataFrame(json.loads(jsonified_df))
+    return df
+
+
+@app.callback(
+    Output("graph", "figure"),
+    [Input("dropdown", "value"), Input("interval", "n_intervals")],
+)
+def update_graph(value, _):
+    df = get_dataframe()
+
+    return {
+        "data": [{"x": df["time"], "y": df["value"], "type": "bar"}],
+        "layout": {"title": value},
+    }
+
+
+@app.callback(
+    Output("status", "children"),
+    [Input("dropdown", "value"), Input("interval", "n_intervals")],
+)
+def update_status(value, _):
+    data_last_updated = redis_instance.hget(
+        tasks.REDIS_HASH_NAME, tasks.REDIS_KEYS["DATE_UPDATED"]
+    ).decode("utf-8")
+    return "Data last updated at {}".format(data_last_updated)
+
+
+if __name__ == "__main__":
     app.run_server(debug=True)
